969b9c623e7a2a34357ae4abdcaefb5a4b8e59a6
ambrosia-fish <josefameur@gmail.com>
2024-07-17 08:54:56 -0400

    Update App.js for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import './App.css';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
