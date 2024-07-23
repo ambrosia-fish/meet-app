67341c7fef02870d818e07da2b6baf82cb703922
ambrosia-fish <josefameur@gmail.com>
2024-07-23 13:39:28 -0400

    Revert "update api.js"
    This reverts commit 32438651a83d3c63ab07f48419ec2fe8b2acf4a3.

--- src/api.js
+++ src/api.js
@@ -1,26 +1,9 @@
-// api.js
-
 import mockData from './mock-data';
 
-const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "arn:aws:lambda:us-east-1:471112996020:function:meet-app-dev-dev-getAuthURL"
-      );
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
+export const extractLocations = (events) => {
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
+  return locations;
 };
 
 const checkToken = async (accessToken) => {
@@ -46,24 +29,11 @@ const removeQuery = () => {
   }
 };
 
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    'arn:aws:lambda:us-east-1:471112996020:function:meet-app-dev-dev-getAccessToken' + '/' + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-};
-
 export const getEvents = async () => {
   if (window.location.href.startsWith("http://localhost")) {
     return mockData;
   }
-
   const token = await getAccessToken();
-
   if (token) {
     removeQuery();
     const url = "arn:aws:lambda:us-east-1:471112996020:function:meet-app-dev-dev-getCalendarEvents" + "/" + token;
@@ -71,8 +41,36 @@ export const getEvents = async () => {
     const result = await response.json();
     if (result) {
       return result.events;
-    } else return null; 
+    } else return null;
   }
 };
 
-export { getAccessToken, getEvents };
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "arn:aws:lambda:us-east-1:471112996020:function:meet-app-dev-dev-getAuthURL"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch(
+    'arn:aws:lambda:us-east-1:471112996020:function:meet-app-dev-dev-getAccessToken' + '/' + encodeCode
+  );
+  const { access_token } = await response.json();
+  access_token && localStorage.setItem("access_token", access_token);
+  return access_token;
+};
