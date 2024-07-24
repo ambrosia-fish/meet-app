82f45a6674723c944dc37f8b871df50775bd8a61
ambrosia-fish <josefameur@gmail.com>
2024-07-24 09:17:36 -0400

    Add integration scope to App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,6 @@
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getEvents } from '../api';
 import App from '../App';
 
 describe('<App /> component', () => {
@@ -18,4 +20,8 @@ describe('<App /> component', () => {
   test('render NumberOfEvents', () => {
     expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
   });
+});
+
+
+describe('<App /> integration', () => {
 });
