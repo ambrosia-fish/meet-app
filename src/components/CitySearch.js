d7799dd74031fa7a0a456ebd90465a99a0988c88
ambrosia-fish <josefameur@gmail.com>
2024-08-01 11:03:50 -0400

    Implemented CitySearch.test.js tests:
    suggestions list is hidden by default
    renders a list of suggestions when city textbox gains focus

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 const CitySearch = () => {
-    return (
-      <div id="city-search">
-        <input
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  return (
+    <div id="city-search">
+      <input
         type="text"
         className="city"
                 placeholder="Search for a city"
-        />
-      </div>
-    )
-  }
-  
-  export default CitySearch;
+        onFocus={() => setShowSuggestions(true)}
+      />
+      {showSuggestions ? <ul className="suggestions"></ul> : null}
+    </div>
+  )
+}
+
+export default CitySearch;
