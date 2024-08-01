b9b243b34b02ad7b10fea0bbe14b5ea1a09e82e3
ambrosia-fish <josefameur@gmail.com>
2024-08-01 11:02:05 -0400

    Updated setupTests.js to ignore issues related to act()

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,21 @@
+// src/setupTests.js
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const MESSAGES_TO_IGNORE = [
+  "When testing, code that causes React state updates should be wrapped into act(...)",
+  "Error:",
+  "The above error occurred",
+  "Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`"
+];
+
+const originalError = console.error;
+
+console.error = (...args) => {
+  const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
+  if (!ignoreMessage) originalError(...args);
+}
