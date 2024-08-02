4cb890fa3fc594f4d5049c5a32080b88415ed1e1
ambrosia-fish <josefameur@gmail.com>
2024-08-02 14:20:25 -0400

    Update Eventlist.test.js

--- src/__tests__/Eventlist.test.js
+++ src/__tests__/Eventlist.test.js
@@ -1,24 +1,30 @@
 import { render, within, waitFor } from '@testing-library/react';
-import EventList from '../components/EventList';
 import { getEvents } from '../api';
+import EventList from '../components/EventList';
 import App from "../App";
 
 describe('<EventList /> component', () => {
- let EventListComponent;
- beforeEach(() => {
-   EventListComponent = render(<EventList />);
- })
+  let EventListComponent;
 
- test('has an element with "list" role', () => {
-   expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
- });
+  test('has an element with "list" role', () => {
+    EventListComponent = render(<EventList events={[]} />);
+    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  });
 
- test('renders correct number of events', async () => {
-    const allEvents = await getEvents(); 
-    EventListComponent.rerender(<EventList events={allEvents} />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length); 
+  test('renders correct number of events', async () => {
+    const allEvents = await getEvents();
+    EventListComponent = render(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+  });
+
+  test('renders empty list when events prop is falsy', () => {
+    EventListComponent = render(<EventList events={null} />);
+    const listElement = EventListComponent.queryByRole("list");
+    expect(listElement).toBeInTheDocument();
+    expect(listElement).toBeEmptyDOMElement();
   });
 });
+
 
 
 describe('<EventList /> integration', () => {
