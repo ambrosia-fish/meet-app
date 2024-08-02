a8eaadd22be64fe7f653f247376aeddd2f77a448
ambrosia-fish <josefameur@gmail.com>
2024-08-02 13:10:15 -0400

    Added test and functionality for 'renders a list of 32 events when the app is mounted and rendered'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
 import './App.css';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
   return (
     <div className="App">
       <CitySearch />
       <NumberOfEvents />
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 }
