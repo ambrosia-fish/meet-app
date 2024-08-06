e416b999265104cec8f9e7df0ad9d76ffb09d257
ambrosia-fish <josefameur@gmail.com>
2024-08-06 17:01:03 -0400

    Updated end to end test for 'User can change the number of events displayed;

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,6 +3,9 @@ import puppeteer from 'puppeteer';
 describe('show/hide an event details', () => {
     let browser;
     let page;
+
+    jest.setTimeout(10000); // Increase Jest timeout to 30 seconds
+
     beforeAll(async () => {
       browser = await puppeteer.launch({
         headless: false,
@@ -13,6 +16,8 @@ describe('show/hide an event details', () => {
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
   });
+  
+
   test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeNull();
@@ -28,6 +33,23 @@ describe('show/hide an event details', () => {
     await page.click('.event .details-btn');
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeNull();
+  });
+
+  test('User can change the number of events displayed', async () => {
+    // Change the number of events to 10
+    await page.focus('#number-of-events-input');
+    await page.keyboard.press('Backspace');
+    await page.keyboard.press('Backspace');
+
+    await page.type('#number-of-events-input', '10');
+    await page.keyboard.press('Enter');
+
+    // Wait for the event list to update
+    await page.waitForSelector('.event');
+
+    // Check if the correct number of events is displayed
+    const events = await page.$$('.event');
+    expect(events.length).toBe(10);
   });
 
 });
