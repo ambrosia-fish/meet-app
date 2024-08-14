73acbf585d81310ead45a09c835c654074d2b08c
ambrosia-fish <josefameur@gmail.com>
2024-08-14 15:30:29 -0400

    Implement errorText logic to NumberOfEvents

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,12 +1,22 @@
 import { useState } from 'react';
 
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
   const [number, setNumber] = useState(32);
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
     setNumber(value);
     setCurrentNOE(Number(value));
+
+    let  errorText;
+    if (value === '' || isNaN(value) || value <= 0) {
+      errorText = "Number of events displayed must be greater than 0."
+    } else {
+      errorText = ""
+    }
+    setErrorAlert(errorText);
+
+
   };
 
   return (
